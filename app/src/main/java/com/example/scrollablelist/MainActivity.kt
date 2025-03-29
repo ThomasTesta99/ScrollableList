15442f5e23a233f69d12b1b924ced26946e18d00
ThomasTesta99 <ttesta99@yahoo.com>
2025-03-29 16:55:00 -0400

    removed imports

--- app/src/main/java/com/example/scrollablelist/MainActivity.kt
+++ app/src/main/java/com/example/scrollablelist/MainActivity.kt
@@ -23,17 +23,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -61,7 +53,6 @@ class MainActivity : ComponentActivity() {
 fun EntertainmentApp(modifier: Modifier = Modifier) {
     Surface(
         modifier = modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
     ) {
         EntertainmentList(Datasource().loadEntertainment())
     }
