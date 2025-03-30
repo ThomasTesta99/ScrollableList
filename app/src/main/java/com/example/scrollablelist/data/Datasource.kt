111ccb3bbdd169b5d4126b440c0f1de004327332
ThomasTesta99 <ttesta99@yahoo.com>
2025-03-30 19:32:55 -0400

    added more images

--- app/src/main/java/com/example/scrollablelist/data/Datasource.kt
+++ app/src/main/java/com/example/scrollablelist/data/Datasource.kt
@@ -11,6 +11,11 @@ class Datasource {
             Entertainment(R.string.image3, R.drawable.image3, R.string.infinityWar),
             Entertainment(R.string.image4, R.drawable.image4, R.string.endgame),
             Entertainment(R.string.image5, R.drawable.image5, R.string.wandavision),
+            Entertainment(R.string.image6, R.drawable.image6, R.string.snyderCut),
+            Entertainment(R.string.image7, R.drawable.image7, R.string.superman),
+            Entertainment(R.string.image8, R.drawable.image8, R.string.doctorStrange2),
+            Entertainment(R.string.image9, R.drawable.image9, R.string.ragnarok),
+            Entertainment(R.string.image10, R.drawable.image10, R.string.mos),
         )
     }
 }
